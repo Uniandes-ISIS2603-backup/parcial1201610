1b74921804f9b38d1034cc495f05911b1df34f5e
Diego F. Ruiz S <yego23@gmail.com>
2016-03-01 16:43:53 -0500

    Versión 3 del parcial
    Usos de $q y promise

--- src/main/webapp/src/modules/person/person.ctrl.js
+++ src/main/webapp/src/modules/person/person.ctrl.js
@@ -3,67 +3,94 @@
 
 (function (ng) {
 // es parte del módulo "personModule"
-      var mod = ng.module("personModule");
-// crea el controlador con dependencias a $scope y a personService
-    mod.controller("personCtrl", ["$scope", "personService", function ($scope, svc) {
-
+    var mod = ng.module("personModule");
+// crea el controlador con dependencias a $scope y a personService y $q
+    mod.controller("personCtrl", ["$scope", "personService", "$q", function ($scope, svc, $q) {
 
             $scope.currentRecord = {};
             $scope.records = [];
-
-            $scope.today = function () {
-                $scope.value = new Date();
-            };
-
-            $scope.clear = function () {
-                $scope.value = null;
-            };
-
-            $scope.open = function ($event) {
-                $event.preventDefault();
-                $event.stopPropagation();
-
-                $scope.opened = true;
-            };
-
             var self = this;
 
             //Variables para el controlador
             this.readOnly = false;
             this.editMode = false;
 
-
             this.changeTab = function (tab) {
                 $scope.tab = tab;
             };
 
+            // función que retorna una promesa para crear a person
+            $scope.savePerson = function (currentRecord) {
+
+                return $q(function (resolve, reject) {
+                    setTimeout(function () {
+                        if (currentRecord !== null) {
+                            svc.saveRecord(currentRecord);
+                            self.fetchRecords();
+                            resolve("Se adiciona el record");  // todo OK
+                        } else {
+                            reject({"message": "El record esta vacío"});//Error
+                        }
+                    }, 1000);
+                });
+            }
+
+            // función que usa la promesa para crear a person
+            $scope.useSavePerson = function () {
+                // obtiene la promesa
+                var promise = $scope.savePerson($scope.currentRecord);
+                // ejecuto la promesa
+                // y cuando termine ejecuta alguna de las dos funciones
+                // del then
+                promise.then(function (respuesta) {
+                    console.debug(respuesta);
+                }, function (error) {
+                    console.debug(error.message);
+                });
+            }
+
+
+            // función que retorna una promesa para editar a person
+            $scope.editPerson = function (record) {
+
+                return $q(function (resolve, reject) {
+                    setTimeout(function () {
+                        if (record !== null) {
+                            svc.fetchRecord(record.id);
+                            self.editMode = true;
+                            $scope.currentRecord = record;
+                            resolve("Se edita el record");  // todo OK
+                        } else {
+                            reject({"message": "El record esta vacío"});//Error
+                        }
+                    }, 1000);
+                });
+            }
+
+            // función que usa la promesa para Editar a person
+            $scope.useEditPerson = function (record) {
+                // obtiene la promesa
+                var promise = $scope.editPerson(record);
+                // ejecuto la promesa
+                // y cuando termine ejecuta alguna de las dos funciones
+                // del then
+                promise.then(function (respuesta) {
+                    console.debug(respuesta);
+                }, function (error) {
+                    console.debug(error.message);
+                });
+            }
 
             this.createRecord = function () {
-
                 $scope.currentRecord = {};
                 this.editMode = true;
             };
-
-
-            this.editRecord = function (record) {
-                svc.fetchRecord(record.id);
-                self.editMode = true;
-                $scope.currentRecord = record;
-
-            };
-
 
             this.fetchRecords = function () {
                 self.editMode = false;
                 $scope.records = svc.fetchRecords();
                 return svc.fetchRecords();
 
-            };
-
-
-            this.saveRecord = function () {
-                svc.saveRecord($scope.currentRecord);
-                self.fetchRecords();
             };
 
             this.deleteRecord = function (record) {
